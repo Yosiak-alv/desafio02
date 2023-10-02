fd4afafd4b54c139328739e9173cf106e16a3963
Yosiak-alv <Josias_romero60@outlook.com>
2023-10-02 16:43:45 -0600

    Update App.js
    Co-Authored-By: Luis Bonilla <124472850+bocchiko@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { NativeWindStyleSheet } from "nativewind";
+import { StackNavigator } from './src/Navigation/Navigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 NativeWindStyleSheet.setOutput({
   default: "native",
@@ -9,8 +11,8 @@ NativeWindStyleSheet.setOutput({
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-green-700">
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <StackNavigator/>
+    </NavigationContainer>
   );
 }
