cbcb8fe450fe62e065e1c63a2800cc1d88868562
Yosiak-alv <Josias_romero60@outlook.com>
2023-10-04 10:08:44 -0600

    Update Navigator.js

--- src/Navigation/Navigator.js
+++ src/Navigation/Navigator.js
@@ -1,6 +1,5 @@
 
 import { createStackNavigator } from "@react-navigation/stack";
-import Register from "../screens/Parts/Register";
 import { PartsList } from "../screens/Parts/PartsList";
 import { CreateParts } from "../screens/Parts/CreateParts";
 
@@ -14,7 +13,6 @@ export const StackNavigator = () => {
         <Stack.Navigator /* screenOptions={screenOptionsStyle} */ >
             
             <Stack.Screen name="PartsList" component={PartsList} initialParams={{level:'',flashMessage:'',part:{}}} />
-            <Stack.Screen name="Register" component={Register} />
             <Stack.Screen name="CreateParts" component={CreateParts} />
         </Stack.Navigator>
     )
